2e7f178e4db24c8cbcd72df9ce7e2db3b392b382
hopeVaughn <hopevaughnwarren@gmail.com>
2023-01-22 10:12:41 -0800

    add moment js dependency link anf finish code

--- darkmode/app.js
+++ darkmode/app.js
@@ -7,10 +7,11 @@ toggleBtn.addEventListener('click', () => {
 const articlesData = articles.map((article) => {
   const { title, date, length, snippet } = article;
   // format date
+  const formatDate = moment(date).format('MMMM Do, YYYY')
   return `<article class="post">
         <h2>${title}</h2>
         <div class="post-info">
-          <span>${date}</span>
+          <span>${formatDate}</span>
           <span>${length} min read</span>
         </div>
         <p>

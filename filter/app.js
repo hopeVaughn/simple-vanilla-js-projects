6dd0d616c98bfb1a438bb76d8fbc50682ebd8c8f
hopeVaughn <hopevaughnwarren@gmail.com>
2023-01-23 09:56:50 -0800

    finish js logic for mini project

--- filter/app.js
+++ filter/app.js
@@ -1,1 +1,68 @@
-console.log('filters project');
+//  grab data
+let filteredProducts = [...products];
+
+// select DOM parent element
+const productsContainer = document.querySelector('.products-container');
+
+// logic for displaying products
+const displayProducts = () => {
+  // conditional
+  if (filteredProducts.length < 1) {
+    productsContainer.innerHTML = `<h5>Sorry, no products matched your search...</h5>`;
+    return
+  }
+
+  productsContainer.innerHTML = filteredProducts.map(({ id, title, image, price }) => {
+    return `<article class="product" data-id="${id}">
+          <img
+            src="${image}"
+            class="product-img img"
+            alt=""
+          />
+          <footer>
+            <h5 class="product-name">${title}</h5>
+            <span class="product-price">${price}</span>
+          </footer>
+        </article>`
+  }).join('');
+};
+displayProducts()
+
+// Text filter for search form
+const form = document.querySelector('.input-form')
+const searchInput = document.querySelector('.search-input')
+
+form.addEventListener('keyup', () => {
+  const inputValue = searchInput.value
+  filteredProducts = products.filter((product) => {
+    return product.title.toLowerCase().includes(inputValue);
+  });
+  displayProducts()
+});
+
+//  button filter
+const companiesDOM = document.querySelector('.companies')
+
+const displayButtons = () => {
+  const buttons = ['all', ...new Set(products.map((product) => product.company))]
+  companiesDOM.innerHTML = buttons.map((company) => {
+    return `    <button class="company-btn" data-id="${company}">${company}</button>`
+  }).join('');
+}
+displayButtons();
+
+//  filters products based on company button click. Clears search input
+companiesDOM.addEventListener('click', (e) => {
+  const el = e.target
+  if (el.classList.contains('company-btn')) {
+    if (el.dataset.id === 'all') {
+      filteredProducts = [...products];
+    } else {
+      filteredProducts = products.filter((product) => {
+        return product.company === el.dataset.id;
+      });
+    }
+    searchInput.value = '';
+    displayProducts()
+  }
+});
